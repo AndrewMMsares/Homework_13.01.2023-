8c0cf4ad757fa1a5195102125a98d1facfe81b31
Andrei Kalinichenko <muse2140@gmail.com>
2024-01-14 20:50:11 +0200

    Basic

---
+++ main.py
@@ -0,0 +1,7 @@
+import re
+
+fio = "Kalinichenko Andrii Andriyovich"
+if re.match(r"^[A-Z][a-z]{1,19}\s[A-Z][a-z]{1,19}\s[A-Z][a-z]{1,19}$", fio):
+    print("Correct")
+else:
+    print("Incorrect")
