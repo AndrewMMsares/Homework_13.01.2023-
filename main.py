eb1791a187e104671e3500ed10e577f5cafba73d
Andrei Kalinichenko <muse2140@gmail.com>
2024-01-14 20:50:11 +0200

    Basic

---
+++ main.py
@@ -0,0 +1,9 @@
+import re
+
+emeils = ["ivan.ivanov@gmail.com", "ivanivanov@gmail.com", "ivanivanov@gmailcom",
+          "ivanivanovgmailcom", "ivanivanovgmail.com", "12ivanivanovgmail.com"]
+for emeil in emeils:
+    if re.match(r"^[a-zA-Z]+[. _]?[a-zA-Z0-9]+@[a-zA-Z]+\.[a-zA-Z]{2,}$", emeil):
+        print(f"emeil {emeil} valid")
+    else:
+        print(f"emeil {emeil} invalid")
